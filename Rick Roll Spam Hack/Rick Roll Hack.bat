d06806b6ed8f8cb80454588324273ec29cf50f05
nathan-patrick <112907807+nathan-patrick@users.noreply.github.com>
2022-10-30 01:58:57 -0400

    Add files via upload
    Downloading the directions file will give you all of the instructions and details you will need to pull off this prank.

---
+++ Rick Roll Spam Hack/Rick Roll Hack.bat
@@ -0,0 +1,9 @@
+(echo MsgBox " Your Computer Is About To Get Rick Rolled!" ,16, "WARNING!")> C:\Users\Public\rickbox.vbs
+start C:\Users\Public\rickbox.vbs
+timeout /t 5
+FOR /L %%A IN (1,1,100) DO (
+	%%A
+	start chrome.exe -new-window "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+
+)
+
